1606490a8f53f35bb07fe0d76db3d78333e6358e
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2016-03-07 20:33:26 +0100

    Upgraded (test) dependencies; fixes #14.

--- src/test/java/nl/talsmasoftware/umldoclet/testing/legacy/EmbeddedAgent.java
+++ src/test/java/nl/talsmasoftware/umldoclet/testing/legacy/EmbeddedAgent.java
@@ -6,7 +6,7 @@ import java.util.EventObject;
  * Created by sjoerd on 07-03-16.
  *
  * @implements java.lang.AutoCloseable    (defined somewhere in classpath)
- * @implements com.api.Agent              (undefined)
+ * @implements com.unavailable.api.Agent  (unavailable on classpath)
  */
 public abstract class EmbeddedAgent {
 
