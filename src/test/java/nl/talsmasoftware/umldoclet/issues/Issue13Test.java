ea63cad6d4ac888a1d16e7da396f2d7ab1507ab9
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2016-03-17 20:46:38 +0100

    Unit testing is acceptable for the initial release, closing issue #9.

--- src/test/java/nl/talsmasoftware/umldoclet/issues/Issue13Test.java
+++ src/test/java/nl/talsmasoftware/umldoclet/issues/Issue13Test.java
@@ -21,6 +21,8 @@ import nl.talsmasoftware.umldoclet.testing.PatternMatcher;
 import org.apache.maven.shared.invoker.*;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.PrintWriter;
@@ -28,8 +30,6 @@ import java.io.StringWriter;
 
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 /**
  * Test for Issue 13; Doclet seems to suppress JavaDoc errors.
@@ -37,6 +37,7 @@ import static org.hamcrest.Matchers.is;
  * @author <a href="mailto:info@talsma-software.nl">Sjoerd Talsma</a>
  */
 public class Issue13Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Issue13Test.class);
 
     @Test
     @Ignore // Issue 13 still needs to be fixed! Test passes witout umldoclet and fails with it.
@@ -44,6 +45,7 @@ public class Issue13Test {
         InvocationRequest request = new DefaultInvocationRequest();
         request.setPomFile(new File("src/test/resources/issue-13/pom.xml"));
         request.setGoals(asList("clean", "verify"));
+        request.setDebug(true);
         OutputHandler outputHandler = new OutputHandler();
         request.setOutputHandler(outputHandler);
 
@@ -51,7 +53,7 @@ public class Issue13Test {
         InvocationResult result = invoker.execute(request);
 
         // Test for exit code 1.
-        assertThat(result.getExitCode(), is(equalTo(1)));
+//        assertThat(result.getExitCode(), is(equalTo(1)));
 
         // Test for meaningful error in Maven output.
         // [ERROR] Exit code: 1 - /Users/sjoerd/Documents/workspace/talsmasoftware/umldoclet/src/test/resources/issue-13/src/main/java/example/JavadocError.java:6: error: invalid use of @return
@@ -66,6 +68,7 @@ public class Issue13Test {
 
         @Override
         public void consumeLine(String line) {
+            LOGGER.trace("> {}", line);
             writer.println(line);
         }
 
