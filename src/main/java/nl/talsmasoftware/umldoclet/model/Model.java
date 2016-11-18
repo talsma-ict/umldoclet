b85674df046fbbc0acc2268004785d425c2aa44f
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2016-11-18 13:19:39 +0100

    Fixed resolution of whether a documented type is an 'iterable' type.

--- src/main/java/nl/talsmasoftware/umldoclet/model/Model.java
+++ src/main/java/nl/talsmasoftware/umldoclet/model/Model.java
@@ -16,12 +16,8 @@
 package nl.talsmasoftware.umldoclet.model;
 
 import com.sun.javadoc.*;
-import com.sun.javadoc.ParameterizedType;
-import com.sun.javadoc.Type;
-import nl.talsmasoftware.umldoclet.logging.LogSupport;
 import nl.talsmasoftware.umldoclet.rendering.Renderer;
 
-import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -29,6 +25,8 @@ import java.util.Set;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableSet;
+import static nl.talsmasoftware.umldoclet.logging.LogSupport.trace;
+import static nl.talsmasoftware.umldoclet.logging.LogSupport.warn;
 
 /**
  * @author Sjoerd Talsma
@@ -36,14 +34,6 @@ import static java.util.Collections.unmodifiableSet;
 public class Model {
     private static final Set<String> OPTIONAL_TYPES = unmodifiableSet(new LinkedHashSet<>(asList(
             "java.util.Optional", "com.google.common.base.Optional")));
-    // TODO: Obviously this is far from ideal! also missing all classes from j.u.concurrent
-    private static final Set<String> ITERABLE_TYPES = unmodifiableSet(new LinkedHashSet<>(asList(
-            "java.lang.Iterable", "java.util.Collection", "java.util.AbstractCollection",
-            "java.util.AbstractList", "java.util.AbstractQueue", "java.util.AbstractSequenctialList",
-            "java.util.AbstractSet", "java.util.Set", "java.util.List", "java.util.Stack",
-            "java.util.SortedSet", "java.util.NavigableSet", "java.util.HashSet",
-            "java.util.TreeSet", "java.util.LinkedHashSet", "java.util.ArrayList",
-            "java.util.LinkedList", "java.util.Vector", "java.util.Queue", "java.util.EnumSet")));
 
     /**
      * Returns whether the the given element is deprecated;
@@ -58,16 +48,14 @@ public class Model {
      */
     public static boolean isDeprecated(ProgramElementDoc element) {
         // Is the element itself deprecated?
-        if (element == null) {
-            return false;
-        } else if (element.tags("deprecated").length > 0) {
-            return true;
-        }
+        if (element == null) return false;
+        else if (element.tags("deprecated").length > 0) return true;
         for (AnnotationDesc annotation : element.annotations()) {
             if (Deprecated.class.getName().equals(annotation.annotationType().qualifiedName())) {
                 return true;
             }
         }
+
         // Element itself is not deprecated.
         // Could it be contained in a deprecated class or extend a deprecated superclass?
         return isDeprecated(element.containingClass())
@@ -78,20 +66,28 @@ public class Model {
      * Return whether tested type is in the same package or a subpackage of the given package name.
      *
      * @param packageName The name of the package to be tested for.
-     * @param testedType  The tested type.
+     * @param type        The tested type.
      * @return whether the tested type is within the specified package.
      */
-    public static boolean isInSameOrSubPackage(String packageName, Type testedType) {
-        if (packageName != null && testedType != null) try {
-            String testedPackageNm = testedType.asClassDoc().containingPackage().name();
+    public static boolean isInSameOrSubPackage(final String packageName, final Type type) {
+        if (packageName != null && type != null) try {
+            String testedPackageNm = type.asClassDoc().containingPackage().name();
             return packageName.equals(testedPackageNm) || testedPackageNm.startsWith(packageName + ".");
         } catch (RuntimeException rte) {
-            LogSupport.warn("Cannot determine whether type \"{0}\" is within the package \"{1}\".",
-                    testedType, packageName, rte);
+            warn("Cannot determine whether type \"{0}\" is within the package \"{1}\".",
+                    type, packageName, rte);
         }
         return false;
     }
 
+    /**
+     * Finds a renderer in a haystack of renderers.
+     *
+     * @param haystack The renderers to search through.
+     * @param needle   The renderer to be found.
+     * @param <R>      The type of the renderer being searched.
+     * @return The found renderer (from the haystack) or <code>null</code> if it couldn't be found.
+     */
     public static <R extends Renderer> R find(Iterable<? extends R> haystack, R needle) {
         if (haystack != null && needle != null) for (R straw : haystack) if (needle.equals(straw)) return straw;
         return null;
@@ -118,65 +114,72 @@ public class Model {
         return null;
     }
 
+    /**
+     * This method returns the parameterized type of the generic iterable type, if it can be determined.<br>
+     * Both Array types and subtypes of {@link Iterable} are supported.
+     *
+     * @param type The type to be inspected whether it is iterable.
+     * @return The type of the iterable, or <code>null</code> if the specified type was not an iterable or that could
+     * not be determined (possibly due to missing JavaDoc or class availability).
+     */
     public static Type iterableType(final Type type) {
-        // check for T[] ...
-        if (type != null && "[]".equals(type.dimension())) {
-            LogSupport.trace("Iterable type found: {0} is an array!", type);
-            return type;
-        }
-        // Next, see if the type is on the classpath and it implements java.lang.Iterable.
-        Type result = reflectParameterizedTypeOf(type, Iterable.class);
-        if (result != null) return result;
+        final String dimension = type != null ? type.dimension() : null;
+        final Type iterableType = dimension != null && dimension.startsWith("[") ? type     // Array
+                : isSubtypeOf(Iterable.class, type) ? firstGenericTypeArgumentOf(type)      // subtype of Iterable
+                : null;
+        trace(iterableType == null ? "Type does not seem to be iterable: {0}."
+                : "Generic type is {1} for iterable {0}.", type, iterableType);
+        return iterableType;
+    }
 
-        // then theck for Iterable<T> ...
-        final List<Type> chain = superclassChainTo(type, new ArrayList<Type>(), ITERABLE_TYPES);
-        if (chain != null) {
-            for (int i = chain.size() - 1; i >= 0; i--) {
-                ParameterizedType genericTp = chain.get(i).asParameterizedType();
-                Type[] typeArgs = genericTp != null ? genericTp.typeArguments() : null;
-                if (typeArgs != null && typeArgs.length == 1) {
-                    return typeArgs[0];
+    /**
+     * This method tries hard to determine whether the specified JavaDoc <code>type</code> is in fact a subtype of the
+     * specified class or interface. Obviously, the class or interface must be on the classpath of the doclet, so this
+     * usually only works for included JDK classes.
+     *
+     * @param javaClass The java class to be checked against as supertype.
+     * @param type      The JavaDoc type to be inspected.
+     * @return <code>true</code> if the JavaDoc type is known to be a subtype of the given class.
+     * <code>false</code> if this is not the case or if it just cannot be determined.
+     */
+    public static boolean isSubtypeOf(final Class<?> javaClass, final Type type) {
+        boolean isSubtype = false;
+        if (javaClass != null && type != null) {
+            if (javaClass.getName().equals(type.typeName())) isSubtype = true;
+            else {
+                final Class<?> typeClass = tryLoadClass(type);
+                if (typeClass != null && javaClass.isAssignableFrom(typeClass)) isSubtype = true;
+                else {
+                    final Type supertype = supertypeOf(type);
+                    isSubtype = supertype != null && isSubtypeOf(javaClass, supertype);
                 }
             }
         }
-        return null;
+        trace("{0} {1} a subtype of {2}.", type, isSubtype ? "is" : "is not", javaClass);
+        return isSubtype;
+    }
+
+    private static Type firstGenericTypeArgumentOf(Type type) {
+        if (type == null) return null;
+        Type genericType = firstGenericTypeArgumentOf(supertypeOf(type));
+        if (genericType == null) {
+            ParameterizedType parameterized = type.asParameterizedType();
+            if (parameterized != null) {
+                Type[] typeArgs = parameterized.typeArguments();
+                if (typeArgs != null && typeArgs.length > 0) genericType = typeArgs[0];
+            }
+        }
+        trace("First generic type is {1} for: {0}.", type, genericType);
+        return genericType;
     }
 
     private static Class<?> tryLoadClass(final Type type) {
         try {
             return Class.forName(type.qualifiedTypeName());
         } catch (ClassNotFoundException | LinkageError | RuntimeException e) {
-            LogSupport.trace("Not a class or unavailable on the classpath: {0}", type);
+            trace("Not a class or unavailable on the classpath: {0}", type);
             return null;
         }
-    }
-
-    private static Type reflectParameterizedTypeOf(final Type type, final Class<?> genericType) {
-        Class<?> result = null;
-        java.lang.reflect.Type _type = tryLoadClass(type);
-        if (_type != null && genericType.isAssignableFrom((Class) _type)) try {
-//            while (!isParameterizedType(_type, genericType)) {
-            while (!(type instanceof java.lang.reflect.ParameterizedType)
-                    || ((java.lang.reflect.ParameterizedType) type).getRawType() != genericType) {
-                LogSupport.trace("Trying to obtain generic superclass of {0}...", _type);
-                _type = _type instanceof java.lang.reflect.ParameterizedType
-                        ? ((Class<?>) ((java.lang.reflect.ParameterizedType) _type).getRawType()).getGenericSuperclass()
-                        : ((Class<?>) _type).getGenericSuperclass();
-            }
-            result = (Class<?>) ((java.lang.reflect.ParameterizedType) _type).getActualTypeArguments()[0];
-            LogSupport.debug("Found iterable generic parameter {0} from {1}.", result, _type);
-        } catch (LinkageError | RuntimeException e) {
-            LogSupport.debug("Error obtaining generic parameter from Iterable {0}: {1}", _type, e.getMessage(), e);
-        }
-        if (result == null) return null;
-        Type foundType = type.asClassDoc().findClass(result.getName());
-        LogSupport.debug("Parameterized type of {0} found from {1}: {2} -> {3}.", genericType, type, result, foundType);
-        return foundType;
-    }
-
-    private static boolean isParameterizedType(java.lang.reflect.Type type, Class<?> declaringClass) {
-        return type instanceof java.lang.reflect.ParameterizedType
-                && declaringClass.equals(((java.lang.reflect.ParameterizedType) type).getRawType());
     }
 
     /**
@@ -188,11 +191,23 @@ public class Model {
      * @return The supertype of the given type, or <code>null</code> if not available.
      */
     public static Type supertypeOf(Type type) {
+        Type supertype = null;
         if (type != null) {
             final ClassDoc classDoc = type.asClassDoc();
-            if (classDoc != null) return classDoc.superclassType();
+            if (classDoc != null) {
+                supertype = classDoc.superclassType();
+                if (supertype == null) { // Could the type actually be on the classpath? (most likely for JDK classes)
+                    final Class<?> clazz = tryLoadClass(type);
+                    final Class<?> superclass = clazz != null ? clazz.getSuperclass() : null;
+                    if (superclass != null) try {
+                        return classDoc.findClass(superclass.getName());
+                    } catch (RuntimeException e) {
+                        trace("Error looking for javadoc of {0}: {1}", superclass, e.toString());
+                    }
+                }
+            }
         }
-        return null;
+        return supertype;
     }
 
     private static List<Type> superclassChainTo(final Type type, List<Type> chain, Set<String> requestedTypes) {
