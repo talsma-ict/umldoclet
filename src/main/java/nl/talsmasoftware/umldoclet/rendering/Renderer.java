68d43dea108b6ea190517c05b676ed261293442b
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-02-27 21:19:43 +0100

    Add automatic license check to build
    Fixes #34.

--- src/main/java/nl/talsmasoftware/umldoclet/rendering/Renderer.java
+++ src/main/java/nl/talsmasoftware/umldoclet/rendering/Renderer.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2016 Talsma ICT
+/**
+ * Copyright 2016-2017 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
