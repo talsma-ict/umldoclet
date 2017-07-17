c88ad1f7d167a53588905343bc5c6f5eb2b16cb3
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-07-17 17:03:54 +0200

    Experiment with java 9 feature.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Look up some jigsaw examples online, e.g.
+ *  https://github.com/cfdobber/maven-java9-jigsaw
+ *  https://github.com/forax/pro
+ */
+module umldoclet {
+
+    requires jdk.javadoc;
+
+}
