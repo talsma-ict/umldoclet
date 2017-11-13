7f2cee42ae16350bdc3bafa849595fbb0c1ce6c4
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2017-11-13 13:55:49 +0100

    Move new classes into new 'model' package

--- src/main/java/nl/talsmasoftware/umldoclet/model/UMLDiagram.java
+++ src/main/java/nl/talsmasoftware/umldoclet/model/UMLDiagram.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package nl.talsmasoftware.umldoclet.uml;
+package nl.talsmasoftware.umldoclet.model;
 
 import nl.talsmasoftware.umldoclet.configuration.Configuration;
 import nl.talsmasoftware.umldoclet.rendering.indent.IndentingPrintWriter;
