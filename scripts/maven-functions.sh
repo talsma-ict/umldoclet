8011b63351ff4f30bde95f76f348a04a30fa90cb
Sjoerd Talsma <talsma.s@tkppensioen.nl>
2018-05-03 12:55:48 +0200

    Add buildscripts for travis

---
+++ scripts/maven-functions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+declare -f debug > /dev/null || source "$(dirname $0)/logging.sh"
+
+#
+# Maven
+#
+
+is_maven_project() {
+    [ -f pom.xml ]
+}
+
+maven_command() {
+#    is_maven_project || fatal "No maven POM file found!"
+    if [ -x ./mvnw ]; then echo "./mvnw";
+    else echo "mvn";
+    fi
+}
+
+get_maven_version() {
+    echo $(printf 'VERSION=${project.version}\n0\n' | $(maven_command) help:evaluate | grep '^VERSION=' | sed 's/VERSION=//')
+}
+
+set_maven_version() {
+    $(maven_command) --batch-mode versions:set versions:commit -DnewVersion="${1}"
+}
+
+build_and_test_maven() {
+    log "Building and Testing project."
+    $(maven_command) --batch-mode clean verify -Dmaven.test.failure.ignore=false
+}
+
+build_and_publish_maven_artifacts() {
+    log "Building and Testing project."
+    $(maven_command) --batch-mode clean verify -Dmaven.test.failure.ignore=false -Dmaven.javadoc.skip=true -Dmaven.source.skip=true
+    log "Publishing project artifacts to maven central."
+    $(maven_command) --batch-mode --no-snapshot-updates -Prelease deploy -DskipTests
+}
