6fe77ff32e62e977e3ef617a824680f220966430
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-05-24 16:35:30 +0200

    Add unit test for DelegatingWriter class

---
+++ src/test/java/nl/talsmasoftware/umldoclet/rendering/writers/DelegatingWriterTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2016-2018 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.umldoclet.rendering.writers;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Sjoerd Talsma
+ */
+public class DelegatingWriterTest {
+
+    @Test
+    public void testEmptyDelegatingWriter() throws IOException {
+        DelegatingWriter delegatingWriter = new DelegatingWriter();
+        delegatingWriter.write("The quick brown fox jumps over the lazy dog");
+        delegatingWriter.flush();
+        delegatingWriter.close();
+        assertThat(delegatingWriter, hasToString("DelegatingWriter[]"));
+    }
+
+    @Test
+    public void testSingleDelegation() throws IOException {
+        StringWriter stringDelegate = new StringWriter();
+        DelegatingWriter delegatingWriter = new DelegatingWriter(stringDelegate);
+        delegatingWriter.write("The quick brown fox jumps over the lazy dog");
+        delegatingWriter.flush();
+        delegatingWriter.close();
+        assertThat(stringDelegate, hasToString("The quick brown fox jumps over the lazy dog"));
+    }
+
+    @Test
+    public void testWrite_singleIOException() throws IOException {
+        IOException ioException = new IOException("IO error!");
+        DelegatingWriter delegatingWriter = new DelegatingWriter(ThrowingWriter.throwing(ioException));
+        try {
+            delegatingWriter.write("The quick brown fox jumps over the lazy dog");
+            fail("IO exception expected");
+        } catch (IOException ioe) {
+            assertThat(ioe, is(sameInstance(ioException)));
+        }
+    }
+
+    @Test
+    public void testWrite_singleRuntimeException() throws IOException {
+        RuntimeException runtimeException = new IllegalStateException("Illegal state!");
+        DelegatingWriter delegatingWriter = new DelegatingWriter(ThrowingWriter.throwing(runtimeException));
+        try {
+            delegatingWriter.write("The quick brown fox jumps over the lazy dog");
+            fail("Runtime exception expected");
+        } catch (RuntimeException rte) {
+            assertThat(rte, is(sameInstance(runtimeException)));
+        }
+    }
+
+    @Test
+    public void testWrite_multipleExceptions() throws IOException {
+        IOException expectedException1 = new IOException("IO error!");
+        RuntimeException expectedException2 = new IllegalStateException("Illegal state!");
+        DelegatingWriter delegatingWriter = new DelegatingWriter(
+                ThrowingWriter.throwing(expectedException1), ThrowingWriter.throwing(expectedException2));
+        try {
+            delegatingWriter.write("The quick brown fox jumps over the lazy dog");
+            fail("IO exception expected");
+        } catch (IOException ioe) {
+            Throwable[] suppressed = ioe.getSuppressed();
+            assertThat(suppressed, is(notNullValue()));
+            assertThat(suppressed.length, is(2));
+            assertThat(suppressed[0], is(sameInstance(expectedException1)));
+            assertThat(suppressed[1], is(sameInstance(expectedException2)));
+        }
+    }
+
+    @Test
+    public void testWrite_combinedSuccessAndException() throws IOException {
+        ThrowingWriter throwingWriter = ThrowingWriter.throwing(new IllegalStateException("Illegal state!"));
+        StringWriter stringWriter = new StringWriter();
+        DelegatingWriter delegatingWriter = new DelegatingWriter(throwingWriter, stringWriter);
+        try {
+            delegatingWriter.write("The quick brown fox jumps over the lazy dog");
+            fail("Exception expected");
+        } catch (RuntimeException expected) {
+            assertThat(expected.getMessage(), is("Illegal state!"));
+        }
+        assertThat(stringWriter, hasToString("The quick brown fox jumps over the lazy dog"));
+    }
+}
---
+++ src/test/java/nl/talsmasoftware/umldoclet/rendering/writers/ThrowingWriter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2016-2018 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.umldoclet.rendering.writers;
+
+import java.io.IOException;
+import java.io.Writer;
+
+/**
+ * Writer that throws exceptions when writing, flushing or closing.
+ *
+ * @author Sjoerd Talsma
+ */
+public class ThrowingWriter extends Writer {
+
+    private Exception exception;
+
+    private ThrowingWriter(Exception exception) {
+        this.exception = exception;
+    }
+
+    static ThrowingWriter throwing(Exception exception) {
+        return new ThrowingWriter(exception);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        if (exception instanceof IOException) throw (IOException) exception;
+        throw (RuntimeException) exception;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        if (exception instanceof IOException) throw (IOException) exception;
+        throw (RuntimeException) exception;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (exception instanceof IOException) throw (IOException) exception;
+        throw (RuntimeException) exception;
+    }
+}
