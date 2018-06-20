50c5ef57d6e0850b115d6ec72d1e8ea4197245f7
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2018-06-20 08:57:19 +0200

    Remove -verbose from test that isn't necessary anymore

--- src/test/java/nl/talsmasoftware/umldoclet/issues/Enhancement25Test.java
+++ src/test/java/nl/talsmasoftware/umldoclet/issues/Enhancement25Test.java
@@ -43,7 +43,6 @@ public class Enhancement25Test {
                 "-d", "target/test-25",
                 "-doclet", UMLDoclet.class.getName(),
                 "-umlImageDirectory", "images",
-                "-verbose",
                 "src/test/java/" + Enhancement25Test.class.getName().replace('.', '/') + ".java"
         );
     }
