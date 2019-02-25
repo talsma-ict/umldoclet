f1d929403ff4662910ce99c358abfa5c174ecbbd
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2019-02-25 10:37:54 +0100

    Create standard javadoc option permutations to be tested

---
+++ src/test/java/nl/talsmasoftware/umldoclet/features/Access.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2016-2019 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.talsmasoftware.umldoclet.features;
+
+public class Access {
+    private static class PrivateClass {
+
+        private String privateField;
+        protected String protectedField;
+        String packageProtectedField;
+        public String publicField;
+
+        private PrivateClass() {
+            this.privateField = protectedField = packageProtectedField = publicField = null;
+        }
+
+        private String getPrivateValue() {
+            return privateField;
+        }
+
+        protected String getProtectedValue() {
+            return protectedField;
+        }
+
+        String getPackageProtectedValue() {
+            return packageProtectedField;
+        }
+
+        public String getPublicValue() {
+            return publicField;
+        }
+    }
+
+    protected static class ProtectedClass {
+        private String privateField;
+        protected String protectedField;
+        String packageProtectedField;
+        public String publicField;
+
+        protected ProtectedClass() {
+            this.privateField = protectedField = packageProtectedField = publicField = null;
+        }
+
+        private String getPrivateValue() {
+            return privateField;
+        }
+
+        protected String getProtectedValue() {
+            return protectedField;
+        }
+
+        String getPackageProtectedValue() {
+            return packageProtectedField;
+        }
+
+        public String getPublicValue() {
+            return publicField;
+        }
+    }
+}
--- src/test/java/nl/talsmasoftware/umldoclet/features/Issue148StandardIncludeOptionsTest.java
+++ src/test/java/nl/talsmasoftware/umldoclet/features/Issue148StandardIncludeOptionsTest.java
@@ -1,11 +1,29 @@
+/*
+ * Copyright 2016-2019 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.talsmasoftware.umldoclet.features;
 
 import nl.talsmasoftware.umldoclet.UMLDoclet;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.spi.ToolProvider;
 
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -22,11 +40,10 @@ import static org.hamcrest.Matchers.is;
  * <li>{@code -private}
  * <li>{@code -protected}
  * <li>{@code -public}
- * <li>{@code --show-module-contents}
- * <li>{@code --show-packages}
- * <li>{@code --show-types}
- * <li>{@code --show-types}
- * <li>{@code --show-members}
+ * <li>{@code --show-module-contents [api|all]}
+ * <li>{@code --show-packages [exported|all]}
+ * <li>{@code --show-types [private|protected|package|all]}
+ * <li>{@code --show-members [private|protected|package|all]}
  * </ul>
  *
  * @author Sjoerd Talsma
@@ -34,15 +51,21 @@ import static org.hamcrest.Matchers.is;
 public class Issue148StandardIncludeOptionsTest {
     private static final File outputdir = new File("target/issues/148");
 
-    private static File createJavadoc(String option) {
-        File dir = new File(outputdir, option);
-        assertThat("Javadoc result", ToolProvider.findFirst("javadoc").get().run(
-                System.out, System.err,
+    private static File createJavadoc(String... options) {
+        File dir = new File(outputdir, String.join("-", options));
+        List<String> args = new ArrayList<>(asList(
                 "-d", dir.getPath(),
                 "-doclet", UMLDoclet.class.getName(),
-                "-quiet", "-createPumlFiles",
-                option,
-                "-sourcepath", "src/test/java", Issue148StandardIncludeOptionsTest.class.getPackageName()
+                "-quiet", "-createPumlFiles"
+        ));
+        args.addAll(asList(options));
+        args.addAll(asList(
+                "-sourcepath", "src/test/java",
+                Issue148StandardIncludeOptionsTest.class.getPackageName()
+        ));
+        assertThat("Javadoc result", ToolProvider.findFirst("javadoc").get().run(
+                System.out, System.err,
+                args.toArray(new String[0])
         ), is(0));
         return dir;
     }
@@ -50,7 +73,81 @@ public class Issue148StandardIncludeOptionsTest {
     @Test
     public void testOptionPrivate() {
         File dir = createJavadoc("-private");
+    }
 
+    @Test
+    public void testOptionProtected() {
+        File dir = createJavadoc("-protected");
+    }
+
+    @Test
+    public void testOptionPackage() {
+        File dir = createJavadoc("-package");
+    }
+
+    @Test
+    public void testOptionPublic() {
+        File dir = createJavadoc("-public");
+    }
+
+    @Test
+    public void testOptionShowModuleContentsApi() {
+        File dir = createJavadoc("--show-module-contents", "api");
+    }
+
+    @Test
+    public void testOptionShowModuleContentsAll() {
+        File dir = createJavadoc("--show-module-contents", "all");
+    }
+
+    @Test
+    public void testOptionShowPackagesExported() {
+        File dir = createJavadoc("--show-packages", "exported");
+    }
+
+    @Test
+    public void testOptionShowPackagesAll() {
+        File dir = createJavadoc("--show-packages", "all");
+    }
+
+    @Test
+    public void testOptionShowTypesPrivate() {
+        File dir = createJavadoc("--show-types", "private");
+    }
+
+    @Test
+    public void testOptionShowTypesProtected() {
+        File dir = createJavadoc("--show-types", "protected");
+    }
+
+    @Test
+    public void testOptionShowTypesPackage() {
+        File dir = createJavadoc("--show-types", "package");
+    }
+
+    @Test
+    public void testOptionShowTypesPublic() {
+        File dir = createJavadoc("--show-types", "public");
+    }
+
+    @Test
+    public void testOptionShowMembersPrivate() {
+        File dir = createJavadoc("--show-members", "private");
+    }
+
+    @Test
+    public void testOptionShowMembersProtected() {
+        File dir = createJavadoc("--show-members", "protected");
+    }
+
+    @Test
+    public void testOptionShowMembersPackage() {
+        File dir = createJavadoc("--show-members", "package");
+    }
+
+    @Test
+    public void testOptionShowMembersPublic() {
+        File dir = createJavadoc("--show-members", "public");
     }
 
 }
