978a657aabe2e5d77561364abfd360a6559ae5d9
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2019-04-26 11:41:47 +0200

    Update license headers for older files
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- src/main/java/nl/talsmasoftware/umldoclet/configuration/Configuration.java
+++ src/main/java/nl/talsmasoftware/umldoclet/configuration/Configuration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 Talsma ICT
+ * Copyright 2016-2019 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
