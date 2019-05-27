2e48b2358db62045b7791c80fb6e81f1a5969be1
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2019-05-27 21:32:55 +0200

    First rudimentary dependency diagram
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- src/main/java/nl/talsmasoftware/umldoclet/uml/DependencyDiagram.java
+++ src/main/java/nl/talsmasoftware/umldoclet/uml/DependencyDiagram.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2016-2019 Talsma ICT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nl.talsmasoftware.umldoclet.uml;
 
 import nl.talsmasoftware.umldoclet.configuration.Configuration;
+import nl.talsmasoftware.umldoclet.rendering.indent.IndentingPrintWriter;
 
 import java.io.File;
 
@@ -20,4 +36,12 @@ public class DependencyDiagram extends Diagram {
         return pumlFile;
     }
 
+    @Override
+    protected <IPW extends IndentingPrintWriter> IPW writeChildrenTo(IPW output) {
+        output.append("set namespaceSeparator none").newline()
+                .append("hide circle").newline()
+                .append("hide empty fields").newline()
+                .append("hide empty methods").newline();
+        return super.writeChildrenTo(output);
+    }
 }
