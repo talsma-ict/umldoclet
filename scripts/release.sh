9eb94e6dd67e30e53bc9a215447ff0560fbd8046
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2019-12-05 20:18:29 +0100

    Update release script to support `develop-v1.1` major-minor versions
    Additionally add some quotes around variables
    
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -eu -o pipefail
-source "$(dirname $0)/options.sh"
+source "$(dirname "$0")/options.sh"
 [[ "${DEBUG:-false}" =~ ^yes|true$ ]] && set -x
 
-declare -f debug > /dev/null || source "$(dirname $0)/logging.sh"
-declare -f is_semantic_version > /dev/null || source "$(dirname $0)/versioning.sh"
-declare -f is_pull_request > /dev/null || source "$(dirname $0)/git-functions.sh"
+declare -f debug > /dev/null || source "$(dirname "$0")/logging.sh"
+declare -f is_semantic_version > /dev/null || source "$(dirname "$0")/versioning.sh"
+declare -f is_pull_request > /dev/null || source "$(dirname "$0")/git-functions.sh"
 
 create_release() {
     fix_travis_fetch
@@ -16,7 +16,8 @@ create_release() {
     local release_version="${branch#*/}"
     debug "Detected version '${release_version}'."
     validate_version "${release_version}"
-    local major_version=$(major_version_of ${release_version})
+    local major_version="$(major_version_of "${release_version}")"
+    local minor_version="$(minor_version_of "${release_version}")"
 
     switch_to_branch "${branch}" || create_branch "${branch}"
     log "Releasing version ${release_version} from branch ${branch}."
@@ -49,9 +50,11 @@ create_release() {
     fi
 
     # Merge to develop and switch to next snapshot
-    local nextSnapshot="$(next_snapshot_version ${release_version})"
+    local nextSnapshot="$(next_snapshot_version "${release_version}")"
     local develop_branch="develop"
-    if [[ $(switch_to_branch "develop-v${major_version}") ]]; then
+    if [[ $(switch_to_branch "develop-v${major_version}.${minor_version}") ]]; then
+        develop_branch="develop-v${major_version}.${minor_version}"
+    elif [[ $(switch_to_branch "develop-v${major_version}") ]]; then
         develop_branch="develop-v${major_version}"
     else
         switch_to_branch "${develop_branch}"
@@ -59,7 +62,7 @@ create_release() {
     [[ "$(get_local_branch)" = "${develop_branch}" ]] || fatal "Could not switch to ${develop_branch} branch."
     log "Merging to ${develop_branch} and updating version to '${nextSnapshot}'."
     git merge --no-edit "${branch}"
-    set_version ${nextSnapshot}
+    set_version "${nextSnapshot}"
     git commit -sam "Release: Set next development version to ${nextSnapshot}"
 
     log "Pushing release to origin and deleting branch '${branch}'."
@@ -74,7 +77,7 @@ create_release() {
 # MAIN
 #----------------------
 
-[ -n "${GIT_BRANCH:-}" ] || GIT_BRANCH=$(find_remote_branch)
+[ -n "${GIT_BRANCH:-}" ] || GIT_BRANCH="$(find_remote_branch)"
 
 if is_release_version "${GIT_BRANCH}"; then
     log "Creating a new release from branch '${GIT_BRANCH}'."
