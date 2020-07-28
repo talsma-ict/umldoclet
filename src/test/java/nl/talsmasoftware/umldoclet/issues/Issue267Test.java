1cc050fe98e84c171d89037a9533cd2a2edcbacf
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2020-07-28 12:56:26 +0200

    Add verification to unit test for issue 267
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- src/test/java/nl/talsmasoftware/umldoclet/issues/Issue267Test.java
+++ src/test/java/nl/talsmasoftware/umldoclet/issues/Issue267Test.java
@@ -21,20 +21,69 @@ import net.sourceforge.plantuml.SourceStringReader;
 import nl.talsmasoftware.umldoclet.util.TestUtil;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class Issue267Test {
 
     @Test
     public void testDiagramWithNestedNamespace() throws IOException {
+        // prepare
         String uml = TestUtil.readUml(getClass().getResourceAsStream("/issue-267-example.puml"));
         File svgDiagram = new File("target/issues/267/example.svg");
         svgDiagram.getParentFile().mkdirs();
         OutputStream out = new FileOutputStream(svgDiagram);
-        new SourceStringReader(uml).outputImage(out, new FileFormatOption(FileFormat.SVG));
+
+        // execute
+        String output = interceptSystemOut(() -> {
+            try {
+                new SourceStringReader(uml).outputImage(out, new FileFormatOption(FileFormat.SVG));
+            } catch (IOException ioe) {
+                fail("I/O error generating image " + svgDiagram, ioe);
+            }
+        }).toLowerCase();
+
+        // verify
+        assertThat(svgDiagram.isFile(), is(true));
+        assertThat(output, not(containsString("error")));
+        assertThat(output, not(containsString("exception")));
+        String svgcontent = TestUtil.read(svgDiagram).toLowerCase();
+        assertThat(svgcontent, not(containsString("error")));
+        assertThat(svgcontent, not(containsString("exception")));
+    }
+
+    private static synchronized String interceptSystemOut(Runnable runnable) {
+        final String charset = StandardCharsets.UTF_8.name();
+        try {
+            PrintStream currentSystemOut = System.out;
+            PrintStream currentSystemErr = System.err;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            try {
+                PrintStream newOut = new PrintStream(baos, true, charset);
+                System.setOut(newOut);
+                System.setErr(newOut);
+                runnable.run();
+                newOut.flush();
+            } finally {
+                System.setOut(currentSystemOut);
+                System.setErr(currentSystemErr);
+            }
+            return baos.toString(charset);
+        } catch (UnsupportedEncodingException unsuppored) {
+            throw new IllegalStateException("Platform doesn't know " + charset + "!", unsuppored);
+        }
     }
 
 }
