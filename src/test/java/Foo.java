6001d6e83d5e8ff4fb192cb35847c8f26bd99839
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2021-03-12 20:03:31 +0100

    Update copyright year.
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- src/test/java/Foo.java
+++ src/test/java/Foo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 Talsma ICT
+ * Copyright 2016-2021 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * Class to test JavaDoc generation from the default package.
  */
