88fa74e75c5e48bba64015bd6bde530277dda3dc
Sjoerd Talsma <sjoerd@talsma-ict.nl>
2021-09-01 12:35:59 +0200

    Add JavaDoc to Visibility.
    Signed-off-by: Sjoerd Talsma <sjoerd@talsma-ict.nl>

--- src/main/java/nl/talsmasoftware/umldoclet/configuration/Visibility.java
+++ src/main/java/nl/talsmasoftware/umldoclet/configuration/Visibility.java
@@ -17,9 +17,36 @@ package nl.talsmasoftware.umldoclet.configuration;
 
 /**
  * The visibility values for classes, methods and fields.
+ * <p>
+ * See <a href="https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html">Access Control</a> description
+ * in the official Oracle documentation.
+ * In the UML Doclet, the visibility is used for two purposes:
+ * <ol>
+ * <li>To represent the visibility of classes, methods and fields in the internal model for rendered diagrams.
+ * <li>To 'ask' the configuration whether a particular diagram should render classes, methods or fields with
+ * a particular visibility.
+ * </ol>
  *
  * @author Sjoerd Talsma
  */
 public enum Visibility {
-    PRIVATE, PROTECTED, PACKAGE_PRIVATE, PUBLIC
+    /**
+     * The visibility corresponding with the Java {@link java.lang.reflect.Modifier#PRIVATE private} modifier.
+     */
+    PRIVATE,
+
+    /**
+     * The visibility corresponding with the Java {@link java.lang.reflect.Modifier#PROTECTED protected} modifier.
+     */
+    PROTECTED,
+
+    /**
+     * The visibility corresponding with the Java default {@link java.lang.reflect.Modifier}.
+     */
+    PACKAGE_PRIVATE,
+
+    /**
+     * The visibility corresponding with the Java {@link java.lang.reflect.Modifier#PUBLIC public} modifier.
+     */
+    PUBLIC
 }
