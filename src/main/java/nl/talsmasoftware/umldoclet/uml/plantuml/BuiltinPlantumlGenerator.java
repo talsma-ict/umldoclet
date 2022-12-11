8aa3dc3fd21ec2689d4728e9223d0b1039ebc794
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2022-12-11 12:20:01 +0100

    Change PlantumlGenerator class to proper interface.
    Signed-off-by: Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>

--- src/main/java/nl/talsmasoftware/umldoclet/uml/plantuml/BuiltinPlantumlGenerator.java
+++ src/main/java/nl/talsmasoftware/umldoclet/uml/plantuml/BuiltinPlantumlGenerator.java
@@ -16,16 +16,15 @@
 package nl.talsmasoftware.umldoclet.uml.plantuml;
 
 import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.SourceStringReader;
 
 import java.io.IOException;
 import java.io.OutputStream;
 
-public interface PlantumlGenerator {
-
-    static PlantumlGenerator autodetect() {
-        return new BuiltinPlantumlGenerator();
+public final class BuiltinPlantumlGenerator implements PlantumlGenerator {
+    @Override
+    public void generatePlantumlDiagramFromSource(String plantumlSource, FileFormat format, OutputStream out) throws IOException {
+        new SourceStringReader(plantumlSource).outputImage(out, new FileFormatOption(format));
     }
-
-    void generatePlantumlDiagramFromSource(String plantumlSource, FileFormat format, OutputStream out) throws IOException;
-
 }
