24e45f9b4933a9a0e0584f90d9ddd15b9179f6cb
Jainish Patel <jn891368@dal.ca>
2024-03-31 15:35:36 -0300

    Refactor Implementation Smell : Complex Method:function refactored in TypeNameWithCardinality class.
    Signed-off-by: Jainish Patel <jn891368@dal.ca>

--- src/main/java/nl/talsmasoftware/umldoclet/javadoc/TypeNameWithCardinality.java
+++ src/main/java/nl/talsmasoftware/umldoclet/javadoc/TypeNameWithCardinality.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022 Talsma ICT
+ * Copyright 2016-2024 Talsma ICT
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,11 +21,7 @@ import javax.lang.model.type.ArrayType;
 import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Types;
-import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 
 import static java.util.Collections.singleton;
@@ -57,7 +53,7 @@ final class TypeNameWithCardinality {
      * @param typeUtils The type utils to use for supertype introspection (required).
      * @return The function to return TypeName with cardinality for use in same-package references.
      */
-    static Function<TypeMirror, TypeNameWithCardinality> function(final Types typeUtils) {
+    /*static Function<TypeMirror, TypeNameWithCardinality> function(final Types typeUtils) {
         requireNonNull(typeUtils, "Type utils are <null>.");
         return type -> {
             if (type instanceof ArrayType) {
@@ -94,5 +90,61 @@ final class TypeNameWithCardinality {
 
             return new TypeNameWithCardinality(TypeNameVisitor.INSTANCE.visit(type), null);
         };
+    }*/
+
+
+    /**
+     * function method was Complex Method. So it is refactored using extract methods to reduce Cyclomatic complexity.
+     */
+    static Function<TypeMirror, TypeNameWithCardinality> function(final Types typeUtils) {
+        requireNonNull(typeUtils, "Type utils are <null>.");
+        return type -> {
+            if (type instanceof ArrayType) {
+                return handleArrayType((ArrayType) type, typeUtils);
+            } else if (type instanceof DeclaredType) {
+                return handleDeclaredType((DeclaredType) type, typeUtils);
+            } else {
+                return new TypeNameWithCardinality(TypeNameVisitor.INSTANCE.visit(type), null);
+            }
+        };
+    }
+
+    private static TypeNameWithCardinality handleArrayType(ArrayType arrayType, Types typeUtils) {
+        TypeName componentName = TypeNameVisitor.INSTANCE.visit(arrayType.getComponentType());
+        return new TypeNameWithCardinality(componentName, "*");
+    }
+
+    private static TypeNameWithCardinality handleDeclaredType(DeclaredType declaredType, Types typeUtils) {
+        Queue<TypeMirror> superTypes = new ArrayDeque<>(Collections.singleton(declaredType));
+        Set<String> checkedTypes = new HashSet<>();
+        while (!superTypes.isEmpty()) {
+            TypeMirror superType = superTypes.poll();
+            String qName = TypeNameVisitor.INSTANCE.visit(superType).qualified;
+            if (checkedTypes.add(qName)) { // Don't reiterate
+                TypeNameWithCardinality typeNameWithCardinality = checkCardinality(superType, qName);
+                if (typeNameWithCardinality != null) return typeNameWithCardinality;
+                superTypes.addAll(typeUtils.directSupertypes(superType));
+            }
+        }
+        return new TypeNameWithCardinality(TypeNameVisitor.INSTANCE.visit(declaredType), null);
+    }
+
+    private static TypeNameWithCardinality checkCardinality(TypeMirror superType, String qualifiedName) {
+        String cardinality;
+        if ("java.util.Optional".equals(qualifiedName) || "com.google.common.base.Optional".equals(qualifiedName)) {
+            cardinality = "0..1";
+        } else if ("java.lang.Iterable".equals(qualifiedName) || "java.util.stream.Stream".equals(qualifiedName)) {
+            cardinality = "*";
+        } else {
+            cardinality = null;
+        }
+
+        // Assumption: the 'iterable' and 'optional' types are DeclaredTypes with a single TypeArgument.
+        Optional<TypeName> typeArgument = Optional.ofNullable(cardinality)
+                .map(c -> superType instanceof DeclaredType ? (DeclaredType) superType : null)
+                .map(DeclaredType::getTypeArguments)
+                .map(args -> args.size() == 1 ? args.get(0) : null)
+                .map(TypeNameVisitor.INSTANCE::visit);
+        return typeArgument.map(ta -> new TypeNameWithCardinality(ta, cardinality)).orElse(null);
     }
 }
