c7dfc83da2f9667e5667d8e84091a4201d31f650
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2024-04-08 12:57:22 +0200

    Remove comment block

--- src/main/java/nl/talsmasoftware/umldoclet/javadoc/TypeNameWithCardinality.java
+++ src/main/java/nl/talsmasoftware/umldoclet/javadoc/TypeNameWithCardinality.java
@@ -53,49 +53,6 @@ final class TypeNameWithCardinality {
      * @param typeUtils The type utils to use for supertype introspection (required).
      * @return The function to return TypeName with cardinality for use in same-package references.
      */
-    /*static Function<TypeMirror, TypeNameWithCardinality> function(final Types typeUtils) {
-        requireNonNull(typeUtils, "Type utils are <null>.");
-        return type -> {
-            if (type instanceof ArrayType) {
-                TypeName componentName = TypeNameVisitor.INSTANCE.visit(((ArrayType) type).getComponentType());
-                return new TypeNameWithCardinality(componentName, "*");
-            } else if (type instanceof DeclaredType) {
-                Queue<TypeMirror> superTypes = new ArrayDeque<>(singleton(type));
-                Set<String> checkedTypes = new HashSet<>();
-                while (!superTypes.isEmpty()) {
-                    TypeMirror superType = superTypes.poll();
-                    String qName = TypeNameVisitor.INSTANCE.visit(superType).qualified;
-                    if (checkedTypes.add(qName)) { // Don't reiterate
-                        String cardinality = null;
-                        if ("java.util.Optional".equals(qName) || "com.google.common.base.Optional".equals(qName)) {
-                            cardinality = "0..1";
-                        } else if ("java.lang.Iterable".equals(qName) || "java.util.stream.Stream".equals(qName)) {
-                            cardinality = "*";
-                        }
-
-                        // Assumption: the 'iterable' and 'optional' types are DeclaredTypes with a single TypeArgument.
-                        Optional<TypeName> typeArgument = Optional.ofNullable(cardinality)
-                                .map(c -> superType instanceof DeclaredType ? (DeclaredType) superType : null)
-                                .map(DeclaredType::getTypeArguments)
-                                .map(args -> args.size() == 1 ? args.get(0) : null)
-                                .map(TypeNameVisitor.INSTANCE::visit);
-                        if (typeArgument.isPresent()) {
-                            return new TypeNameWithCardinality(typeArgument.get(), cardinality);
-                        }
-
-                        superTypes.addAll(typeUtils.directSupertypes(superType));
-                    }
-                }
-            }
-
-            return new TypeNameWithCardinality(TypeNameVisitor.INSTANCE.visit(type), null);
-        };
-    }*/
-
-
-    /**
-     * function method was Complex Method. So it is refactored using extract methods to reduce Cyclomatic complexity.
-     */
     static Function<TypeMirror, TypeNameWithCardinality> function(final Types typeUtils) {
         requireNonNull(typeUtils, "Type utils are <null>.");
         return type -> {
